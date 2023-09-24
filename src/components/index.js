61d058070822664ab462534aa29943de058e5f63
Sadjed <77120200+sajeed11@users.noreply.github.com>
2023-09-24 16:30:19 +0100

    Day One

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import NavBar from "./NavBar";
+import Button from "./Button";
+import Business from "./Business";
+import Billing from "./Billing";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import Footer from "./Footer";
+import FeedbackCard from "./FeedbackCard";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Hero from "./Hero";
+import Testimonials from "./Testimonials";
+
+export {
+  NavBar,
+  Billing,
+  Business,
+  Hero,
+  Testimonials,
+  Stats,
+  FeedbackCard,
+  Footer,
+  CTA,
+  CardDeal,
+  Clients,
+  GetStarted,
+  Button,
+};
