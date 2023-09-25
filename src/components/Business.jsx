6c7e8b75531ef2d14ac36bd44895ee8c472a7128
Sadjed <77120200+sajeed11@users.noreply.github.com>
2023-09-25 21:39:12 +0100

    Third Day

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Buttons from "./Button";
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business,
+          <br className="sm:block hidden" />
+          we will handle the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px]`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Business;
